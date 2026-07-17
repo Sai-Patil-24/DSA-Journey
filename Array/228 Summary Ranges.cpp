7bd1ff573a0dffce24ad2988dcb781e3faf8bf2c
Sai <saipatil55603@gmail.com>
2026-07-17 23:21:59 +0530

    dsa-update

--- Array/228 Summary Ranges.cpp
+++ Array/228 Summary Ranges.cpp
@@ -48,3 +48,30 @@ public:
         return result;
     }
 };
+
+
+
+class Solution {
+public:
+    vector<string> summaryRanges(vector<int>& nums) {
+        vector<string> result;
+        int n = nums.size();
+
+        int i = 0;
+        while (i < n) {
+            int start = nums[i];
+
+            while (i < n - 1 && nums[i + 1] == nums[i] + 1)
+                i++;
+
+            if (start == nums[i])
+                result.push_back(to_string(start));
+            else
+                result.push_back(to_string(start) + "->" + to_string(nums[i]));
+
+            i++;
+        }
+
+        return result;
+    }
+};
