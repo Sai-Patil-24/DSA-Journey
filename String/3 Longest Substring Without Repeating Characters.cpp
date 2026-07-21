fbe44d54eaa57a1f2445ec1c618f59f20b879643
Sai <saipatil55603@gmail.com>
2026-07-21 20:56:03 +0530

    dsa-update

---
+++ String/3 Longest Substring Without Repeating Characters.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        int maxLen = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            unordered_set<char> seen;
+            for (int j = i; j < s.length(); j++) {
+                if (seen.count(s[j])) {
+                    
+                    break;
+                }
+                seen.insert(s[j]);
+                maxLen = max(maxLen, j - i + 1);
+            }
+        }
+
+        return maxLen;
+    }
+};
