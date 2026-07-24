8c600ff0ce1e3e3304a5b847dc95891370e312e3
Sai <saipatil55603@gmail.com>
2026-07-24 22:04:21 +0530

    dsa-update

--- Sorting/75 Sort Colors.cpp
+++ Sorting/75 Sort Colors.cpp
@@ -11,3 +11,38 @@ public:
     }
 };
 
+
+/*
+ * LeetCode Problem: 75. Sort Colors
+ * Link: https://leetcode.com/problems/sort-colors/
+ *
+ * Approach: Dutch National Flag Algorithm
+ * - Use three pointers (`red`, `white`, `blue`) to partition the array.
+ * - Place 0s at the beginning, 2s at the end, and leave 1s in the middle.
+ * - Sort the array in a single pass using constant extra space.
+ *
+ * Time Complexity: O(n)
+ * Space Complexity: O(1)
+ */
+
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int red = 0;
+        int white = 0;
+        int blue = nums.size() - 1;
+
+        while (white <= blue) {
+            if (nums[white] == 0) {
+                swap(nums[white], nums[red]);
+                red++;
+                white++;
+            } else if (nums[white] == 1) {
+                white++;
+            } else {
+                swap(nums[white], nums[blue]);
+                blue--;
+            }
+        }
+    }
+};
