f8262c8ce1eaa35e3110a8d6c44056cef9a5a51e
Sai <saipatil55603@gmail.com>
2026-07-27 21:13:15 +0530

    dsa-update

--- Array/1464 Maximum Product of Two Elements in an Array.cpp
+++ Array/1464 Maximum Product of Two Elements in an Array.cpp
@@ -21,3 +21,39 @@ public:
         return (nums[n - 1] - 1) * (nums[n - 2] - 1);
     }
 };
+
+
+
+/*
+    Author: Sai Patil
+    Problem: 1464. Maximum Product of Two Elements in an Array
+    Platform: LeetCode
+    Link: https://leetcode.com/problems/maximum-product-of-two-elements-in-an-array/
+
+    Approach:
+    1. Traverse the array once to find the largest and second largest elements.
+    2. Update both values whenever a larger element is found.
+    3. Compute and return (largest - 1) * (secondLargest - 1).
+
+    Time Complexity: O(n)
+    Space Complexity: O(1)
+*/
+
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) {
+        int max1 = INT_MIN;
+        int max2 = INT_MIN;
+
+        for (int i : nums) {
+            if (i > max1) {
+                max2 = max1;
+                max1 = i;
+            } else if (i > max2) {
+                max2 = i;
+            }
+        }
+
+        return (max1 - 1) * (max2 - 1);
+    }
+};
