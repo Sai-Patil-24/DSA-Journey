b52a4a01e074f5067dd137b19d085470c0a04ac4
Sai Patil <saipatil55603@gmail.com>
2026-08-19 18:56:08 +0530

    dsa-update

--- String/1689. Partitioning Into Minimum Number Of Deci-Binary Numbers.cpp
+++ String/1689. Partitioning Into Minimum Number Of Deci-Binary Numbers.cpp
@@ -28,3 +28,19 @@ public:
     }
 };
 
+
+//optimal approach 
+
+class Solution {
+public:
+    int minPartitions(string n) {
+        char mx = '0';
+
+        for (char c : n) {
+            mx = max(mx, c);
+        }
+
+        return mx - '0';
+    }
+};
+
