6868d943bcaa1b2e8b8f31b12234f400bdf40f18
Sai Patil <saipatil55603@gmail.com>
2026-09-01 20:20:53 +0530

    dsa-update

--- Hash/30. Substring with Concatenation of All Words.cpp
+++ Hash/30. Substring with Concatenation of All Words.cpp
@@ -40,3 +40,75 @@ public:
 };
 
 
+//optimized approach
+class Solution {
+public:
+    vector<int> findSubstring(string s, vector<string>& words) {
+
+        int n = s.length();
+        int wordLen = words[0].length();
+        int wordCount = words.size();
+        int totalLen = wordLen * wordCount;
+
+        vector<int> ans;
+
+        unordered_map<string, int> required;
+
+       
+        for (string word : words) {
+            required[word]++;
+        }
+
+       
+        for (int offset = 0; offset < wordLen; offset++) {
+
+            int left = offset;
+            int right = offset;
+            int count = 0;
+
+            unordered_map<string, int> current;
+
+            while (right + wordLen <= n) {
+
+                
+                string word = s.substr(right, wordLen);
+                right += wordLen;
+
+                
+                if (required.find(word) == required.end()) {
+                    current.clear();
+                    count = 0;
+                    left = right;
+                    continue;
+                }
+
+              
+                current[word]++;
+                count++;
+
+              
+                while (current[word] > required[word]) {
+
+                    string leftWord = s.substr(left, wordLen);
+
+                    current[leftWord]--;
+                    left += wordLen;
+                    count--;
+                }
+
+                
+                if (count == wordCount) {
+                    ans.push_back(left);
+
+                   
+                    string leftWord = s.substr(left, wordLen);
+                    current[leftWord]--;
+                    left += wordLen;
+                    count--;
+                }
+            }
+        }
+
+        return ans;
+    }
+};
