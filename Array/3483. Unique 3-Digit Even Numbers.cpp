90d6361822a53158479fcf3161f1b91e8e62213c
Sai Patil <saipatil55603@gmail.com>
2026-09-11 12:35:23 +0530

    dsa-update

---
+++ Array/3483. Unique 3-Digit Even Numbers.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int totalNumbers(vector<int>& digits) {
+        int num=0;
+        unordered_set<int> st;
+        for (int i=0; i<digits.size(); i++){
+             if(digits[i]!=0){
+                for (int j =0 ; j<digits.size(); j++){
+                    for (int k=0; k < digits.size();k++){
+                        if(i != j && j != k && i != k){
+                            num=digits[i]*100 + digits[j]*10 + digits[k];
+                            if(num%2==0) st.insert(num);   
+                        }
+                       
+                    }
+                }
+             }
+                
+        }
+
+        return st.size();
+    }
+};
